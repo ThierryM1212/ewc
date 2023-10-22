1f0bb38f105b798dd95885dcd416c0f2b5ffc63b
ThierryM1212 <thierry.mangiante@gmail.com>
2023-10-23 00:32:05 +0200

    fix cleanup in git workflow

--- tests/teardown-test.js
+++ tests/teardown-test.js
@@ -1,23 +1,29 @@
 let fs = require('fs');
 
+function deleteIfExists(filePath) {
+    if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+    }
+}
+
 // cleanup after tests
 module.exports = async () => {
-    fs.unlinkSync('build/wallets/test.wallet');
-    fs.unlinkSync('build/wallets/test26.wallet');
-    fs.unlinkSync('build/wallets/test123.wallet');
-    fs.unlinkSync('build/wallets/testT.wallet');
-    fs.unlinkSync('build/wallets/testWallet_empty_account_list.wallet');
-    fs.unlinkSync('build/wallets/testWallet_empty_address_list.wallet');
-    fs.unlinkSync('build/wallets/testWallet_invalid.wallet');
-    fs.unlinkSync('build/wallets/testWallet0.wallet');
-    fs.unlinkSync('build/wallets/testWallet1.wallet');
-    fs.unlinkSync('build/wallets/testWallet1updated.wallet');
-    fs.unlinkSync('build/wallets/testWallet3.wallet');
-    fs.unlinkSync('build/wallets/testWallet4.wallet');
-    fs.unlinkSync('build/wallets/testWalletEWC0.wallet');
-    fs.unlinkSync('build/wallets/testWalletEWC1.wallet');
-    fs.unlinkSync('build/wallets/testWalletNSC1.wallet');
-    fs.unlinkSync('build/wallets/testWalletNWC1.wallet');
-    fs.unlinkSync('build/wallets/testWalletNWC2.wallet');
+    deleteIfExists('build/wallets/test.wallet');
+    deleteIfExists('build/wallets/test26.wallet');
+    deleteIfExists('build/wallets/test123.wallet');
+    deleteIfExists('build/wallets/testT.wallet');
+    deleteIfExists('build/wallets/testWallet_empty_account_list.wallet');
+    deleteIfExists('build/wallets/testWallet_empty_address_list.wallet');
+    deleteIfExists('build/wallets/testWallet_invalid.wallet');
+    deleteIfExists('build/wallets/testWallet0.wallet');
+    deleteIfExists('build/wallets/testWallet1.wallet');
+    deleteIfExists('build/wallets/testWallet1updated.wallet');
+    deleteIfExists('build/wallets/testWallet3.wallet');
+    deleteIfExists('build/wallets/testWallet4.wallet');
+    deleteIfExists('build/wallets/testWalletEWC0.wallet');
+    deleteIfExists('build/wallets/testWalletEWC1.wallet');
+    deleteIfExists('build/wallets/testWalletNSC1.wallet');
+    deleteIfExists('build/wallets/testWalletNWC1.wallet');
+    deleteIfExists('build/wallets/testWalletNWC2.wallet');
 
 };
